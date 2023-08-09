90f943d2b547eb40527c17337c18c37e0eba5259
eumssooo <mamma1115@gmail.com>
2023-08-10 00:06:03 +0900

    ✨ 회원 가입 로직 기능  구현
    프론트엔드 및 백엔드 작업 (signup)

--- main.py
+++ main.py
@@ -54,4 +54,21 @@ async def get_image(item_id):
     return Response(content=bytes.fromhex(image_bytes))
 
 
+@app.post("/signup")
+def signup(
+    id: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    name: Annotated[str, Form()],
+    email: Annotated[str, Form()],
+):
+    cur.execute(
+        f"""
+                INSERT INTO users ('id', 'password', 'name', 'email')
+                VALUES ('{id}', '{password}','{name}','{email}')
+                """
+    )
+    con.commit()
+    return "200"
+
+
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
